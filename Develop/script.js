6bfce62d32f4a52c8ec735423569fd2da346733a
BondT1 <tombond248@gmail.com>
2023-04-20 19:53:46 +0100

    added prompts to verify password length

--- Develop/script.js
+++ Develop/script.js
@@ -10,10 +10,29 @@ const specialCharacters = " !#$%&'()*+,-./:;<=>?@][\^_`{|}~";
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+// Prompts user to verify password length (between 8 and 128 characters)
+
+function verifyLength() {
+  var verifyLength = prompt("Please specify how many characters you want your password to contain. (8 - 128 characters)");
+
+  // verify length is a valid number.
+  while (isNaN(verifyLength)) {
+    alert("Enter a valid number.");
+    var verifyLength = prompt("Please specify how many characters you want your password to contain. (8 - 128 characters)");
+  }
+  
+  // verify length is between 8 and 128 characters.
+  while (verifyLength < 8 || verifyLength > 128) {
+    alert("Password length must be between 8 and 128 characters");
+    var verifyLength = prompt("Please specify how many characters you want your password to contain. (8 - 128 characters)");
+  } 
+  
+  return Number(verifyLength);
+} 
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
